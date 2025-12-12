9ee1c926d86d337cec918878bec7753320b1fefc
jaenam615 <jaenam2060@gmail.com>
2025-12-13 03:43:45 +0900

    feat: init server

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
---
+++ src/main/kotlin/com/example/jaebitly/JaebitlyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jaebitly
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JaebitlyApplication
+
+fun main(args: Array<String>) {
+    runApplication<JaebitlyApplication>(*args)
+}
