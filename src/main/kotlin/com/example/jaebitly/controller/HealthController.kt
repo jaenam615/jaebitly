d0a2e0e346614de58df2b7ba53bb98caab932dea
jaenam615 <jaenam2060@gmail.com>
2025-12-13 04:06:19 +0900

    feat: structure of repository

--- src/main/kotlin/com/example/jaebitly/controller/HealthController.kt
+++ src/main/kotlin/com/example/jaebitly/controller/HealthController.kt
@@ -1,4 +1,4 @@
-package com.example.jaebitly
+package com.example.jaebitly.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
