555085e1a115fcfe09d69d43bfd6b277b395d0fc
jaenam615 <jaenam2060@gmail.com>
2025-12-13 18:38:41 +0900

    feat: async redirect event publishing

--- src/main/kotlin/com/example/jaebitly/infrastructure/LogRedirectEventPublisher.kt
+++ src/main/kotlin/com/example/jaebitly/infrastructure/LogRedirectEventPublisher.kt
@@ -1,16 +1,23 @@
 package com.example.jaebitly.infrastructure
 
 import com.example.jaebitly.application.RedirectEventPublisher
-import com.example.jaebitly.domain.ShortKey
 import com.example.jaebitly.domain.event.RedirectEvent
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
 class LogRedirectEventPublisher : RedirectEventPublisher {
     private val log = LoggerFactory.getLogger(javaClass)
 
+    @Async
     override fun publish(event: RedirectEvent) {
-        log.info("redirect_event shortKey={}, occurredAt{}", event.shortKey, event.occurredAt)
+        log.info(
+            "redirect_event shortKey={}, occurredAt={}, ip={}, user-agent={}",
+            event.shortKey,
+            event.occurredAt,
+            event.ip,
+            event.userAgent,
+        )
     }
 }
--- src/test/kotlin/com/example/jaebitly/RedirectIntegrationTest.kt
+++ src/test/kotlin/com/example/jaebitly/RedirectIntegrationTest.kt
@@ -1,11 +1,15 @@
 package com.example.jaebitly
 
-import com.example.jaebitly.JaebitlyApplication
+import com.example.jaebitly.application.RedirectEventPublisher
 import com.jayway.jsonpath.JsonPath
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.timeout
+import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
@@ -18,35 +22,68 @@ class RedirectIntegrationTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @MockBean
+    lateinit var redirectEventPublisher: RedirectEventPublisher
+
     @Test
     fun `invalid shortKey returns 404`() {
-        mockMvc.get("/invalid-key")
+        mockMvc
+            .get("/invalid-key")
             .andExpect {
                 status { isNotFound() }
             }
     }
 
     @Test
-    fun `valid shortKey redirects with 302`(){
+    fun `valid shortKey redirects with 302`() {
         // given: create link
-        val createResponse = mockMvc.post("/links"){
-            contentType = MediaType.APPLICATION_JSON
-            content = """{"originalUrl" : "https://example.com"}"""
-        }.andExpect {
-            status { isOk() }
-        }.andReturn()
-
-        println(createResponse)
-        println(createResponse.response.contentAsString)
+        val createResponse =
+            mockMvc
+                .post("/links") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = """{"originalUrl" : "https://example.com"}"""
+                }.andExpect {
+                    status { isOk() }
+                }.andReturn()
 
         val responseBody = createResponse.response.contentAsString
         val shortKey = JsonPath.read<String>(responseBody, "$.shortKey")
 
         // when & then: redirect
-        mockMvc.get("/$shortKey")
+        mockMvc
+            .get("/$shortKey")
             .andExpect {
                 status { isFound() }
                 header { string(HttpHeaders.LOCATION, "https://example.com") }
             }
+    }
+
+    @Test
+    fun `redirect triggers event publishing async`() {
+        // given: create link
+        val createResponse =
+            mockMvc
+                .post("/links") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = """{"originalUrl" : "https://example.com"}"""
+                }.andExpect {
+                    status { isOk() }
+                }.andReturn()
+
+        val responseBody = createResponse.response.contentAsString
+        val shortKey = JsonPath.read<String>(responseBody, "$.shortKey")
+
+        // when: redirect
+        mockMvc
+            .get("/$shortKey")
+            .andExpect {
+                status { isFound() }
+            }
+
+        //  then: event async
+        verify(
+            redirectEventPublisher,
+            timeout(1000),
+        ).publish(any())
     }
 }
